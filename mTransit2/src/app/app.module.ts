0b5faa9486bdd02de193ed0e7a8303e0ddea87ae
Kenny Nguyen <alphakennyn@gmail.com>
2017-02-21 20:34:32 -0500

    integrate AUTH0

--- mTransit2/src/app/app.module.ts
+++ mTransit2/src/app/app.module.ts
@@ -3,6 +3,20 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { loginPage } from '../pages/login/login';
 
+import { AuthConfig, AuthHttp } from 'angular2-jwt';
+import { AuthService } from '../services/auth/auth.service';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
+
+let storage: Storage = new Storage();
+
+export function getAuthHttp(http) {
+  return new AuthHttp(new AuthConfig({
+    globalHeaders: [{'Accept': 'application/json'}],
+    tokenGetter: (() => storage.get('id_token'))
+  }), http);
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -16,6 +30,11 @@ import { loginPage } from '../pages/login/login';
     MyApp,
     loginPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},AuthService,
+    {
+      provide: AuthHttp,
+      useFactory: getAuthHttp,
+      deps: [Http]
+    }]
 })
 export class AppModule {}
--- mTransit2/src/pages/login/login.ts
+++ mTransit2/src/pages/login/login.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {AuthService} from '../../services/auth/auth.service';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
+
 export class loginPage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
+  constructor(public navCtrl: NavController, public auth: AuthService) {}
 
 }
