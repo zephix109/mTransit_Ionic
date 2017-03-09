bc804f10be335310cba6cd45ce92faa81078c7aa
Michael Mescheder <meschedermichael@gmail.com>
2017-03-09 00:20:52 -0500

    Remove SMS Files

--- mTransit/src/app/app.component.ts
+++ mTransit/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-import { AuthService } from '../services/auth/auth.service';
 import { HomePage } from '../pages/home/home';
 
 
@@ -11,19 +10,15 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
-  constructor(platform: Platform ,private auth: AuthService) {
+  constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
-
-      auth.startupTokenRefresh();
       
     });
   }
-
-
 
 }
 
