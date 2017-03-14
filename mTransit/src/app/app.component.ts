f284a5c7b7b03c1d6743cbb5a1b0e2dfb0fdca11
Andrew Laramee <zephix109@gmail.com>
2017-03-13 22:11:56 -0400

    Minor text updates

--- mTransit/src/app/app.component.ts
+++ mTransit/src/app/app.component.ts
@@ -26,11 +26,8 @@ export class MyApp {
     firebase.initializeApp(firebaseConfig);
 
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
-      
     });
   }
 
