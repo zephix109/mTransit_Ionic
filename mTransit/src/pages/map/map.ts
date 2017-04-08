ef2cc8f645b8c599a125f06c39e9cd65ff952ae8
Andrew Laramee <zephix109@gmail.com>
2017-04-08 14:36:51 -0400

    Minor refactoring to map.ts
    Changes to improve code smell

--- mTransit/src/pages/map/map.ts
+++ mTransit/src/pages/map/map.ts
@@ -1,38 +1,38 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { BusStopService } from '../../providers/bus-stop-service';
 import { GoogleMaps } from '../../providers/google-maps';
 import { NavController, Platform } from 'ionic-angular';
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { RatingPagePage } from '../rating-page/rating-page';
  
 @Component({
-  selector: 'page-map',
-  templateUrl: 'map.html'
+  selector: "page-map",
+  templateUrl: "map.html"
 })
 export class MapPage {
  
-  @ViewChild('map') public mapElement: ElementRef;
-  @ViewChild('pleaseConnect') public pleaseConnect: ElementRef;
+  @ViewChild("map") public mapElement: ElementRef;
+  @ViewChild("pleaseConnect") public pleaseConnect: ElementRef;
 
   public map: any;
   public mapInitialised: boolean = false;
 
-  constructor(public navCtrl: NavController, public maps: GoogleMaps, public platform: Platform, public bus_stop_service: BusStopService) {}
+  constructor(public navCtrl: NavController, public maps: GoogleMaps, public platform: Platform, public busStopService: BusStopService) {}
  
-  public ionViewDidLoad(){
+  public ionViewDidLoad() {
     this.platform.ready().then(() => {
         const mapLoaded = this.maps.init(this.mapElement.nativeElement, this.pleaseConnect.nativeElement);
-        const stopLoaded = this.bus_stop_service.load_Near_User();
-        const input = document.getElementById('searchInput');       
+        const stopLoaded = this.busStopService.load_Near_User();
+        const input = document.getElementById("searchInput");       
 
         Promise.all([
           mapLoaded,
           stopLoaded
         ]).then((result) => {
-          this.maps.map.addListener('click', (pos) =>{
+          this.maps.map.addListener("click", (pos) => {
             this.maps.selectedPath = false;
             this.maps.clearDisplayedPaths();
-            this.bus_stop_service.load_Destination(pos.latLng.lat(),pos.latLng.lng()).then(() => {
+            this.busStopService.load_Destination(pos.latLng.lat(),pos.latLng.lng()).then(() => {
               this.maps.showMarkers(result);
             });
           });
@@ -42,7 +42,7 @@ export class MapPage {
     });
   } 
         
-  public goToRating(){
+  public goToRating() {
     this.navCtrl.push(RatingPagePage);
   }
 }
