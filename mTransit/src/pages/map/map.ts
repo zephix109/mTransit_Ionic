d0aa744715ba9193fc7a23adb5863c5feeefe116
Andrew Laramee <zephix109@gmail.com>
2017-04-10 01:03:18 -0400

    Stage 5 refactoring
    map.ts

--- mTransit/src/pages/map/map.ts
+++ mTransit/src/pages/map/map.ts
@@ -18,17 +18,17 @@ export class MapPage {
 
   map: any;
   mapInitialised: boolean = false;
- 
+
   constructor(public navCtrl: NavController, public maps: GoogleMaps, public platform: Platform, public bus_stop_service: BusStopService) { }
 
-  public ionViewDidLoad(){
+  public ionViewDidLoad() {
 
     //console.log("my map is "+this.maps);
     var input = document.getElementById("searchInput");
     var gomap = this.maps;
-    var montrealBounds = new google.maps.LatLngBounds( 
-        new google.maps.LatLng(45.383291, -74.011961),
-        new google.maps.LatLng(45.716133, -73.447467) 
+    var montrealBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(45.383291, -74.011961),
+      new google.maps.LatLng(45.716133, -73.447467)
     );
     var options = {
       strictBounds: true,
@@ -36,35 +36,35 @@ export class MapPage {
     }
 
     var autocomplete = new google.maps.places.Autocomplete(input, options);
-    autocomplete.addListener('place_changed', function() {
-      
-    gomap.goToPlace(autocomplete.getPlace())
-    
-  });
-    
+    autocomplete.addListener('place_changed', function () {
+
+      gomap.goToPlace(autocomplete.getPlace())
+
+    });
+
     this.platform.ready().then(() => {
- 
-        let mapLoaded = this.maps.init(this.mapElement.nativeElement, this.pleaseConnect.nativeElement);
-        let stopLoaded = this.bus_stop_service.load_Near_User();       
 
-        Promise.all([
-          mapLoaded,
-          stopLoaded
-      
-        ]).then((result) => {
+      let mapLoaded = this.maps.init(this.mapElement.nativeElement, this.pleaseConnect.nativeElement);
+      let stopLoaded = this.bus_stop_service.load_Near_User();
 
-          //console.log("Result length = " + result[1].length);
-          this.maps.map.addListener('click', (pos) =>{
-            this.maps.selectedPath = false;
-            this.maps.clearDisplayedPaths();
-            this.bus_stop_service.load_Destination(pos.latLng.lat(),pos.latLng.lng()).then((result) => {
-              this.maps.showMarkers(result);
-            });
+      Promise.all([
+        mapLoaded,
+        stopLoaded
+
+      ]).then((result) => {
+
+        //console.log("Result length = " + result[1].length);
+        this.maps.map.addListener('click', (pos) => {
+          this.maps.selectedPath = false;
+          this.maps.clearDisplayedPaths();
+          this.bus_stop_service.load_Destination(pos.latLng.lat(), pos.latLng.lng()).then((result) => {
+            this.maps.showMarkers(result);
           });
-      }).catch( rej => {
-          console.log(rej);
         });
+      }).catch(rej => {
+        console.log(rej);
       });
+    });
   }
 
   public goToRating() {
